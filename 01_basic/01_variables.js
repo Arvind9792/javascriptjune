870741418930c6ae52a7a482346ef4b520991987
Arvind9792 <117763350+Arvind9792@users.noreply.github.com>
2023-11-04 14:40:58 +0000

    datatype

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=1234
+var accountEmail="arvindkumar@gmail.com"
+let accountPassword="123123"
+accountCity="kanpur"
+//accountId=1
+accountEmail="saviat@jhfds.com"
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountEmail,accountPassword,accountId,accountCity])
