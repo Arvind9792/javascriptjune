ae8f2bb517ee3d1f9ff80a77018c48662cb3010e
Arvind9792 <117763350+Arvind9792@users.noreply.github.com>
2023-11-06 11:29:33 +0000

    datatype done

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,14 @@
+/* Data Type : Primitive data Type(call By Value)
+Non primitive data type (Call By Refrence)
+Primitive Data Type: number,string,boolean,null,bigInt,symbol,undifine
+Non-primitive Data Type: Array,Object,Function
+*/
+const value=101 //number
+const newValue=101.3 //Nunber
+const name="Arvind" //String
+let age; //undifine
+let temp=null //null
+let id=Symbol('123')
+let anotherId=Symbol('123')
+console.log(id===anotherId);
+
