a7ac9233b973fc27166aee130f13cfd05a0c8c22
Arvind9792 <117763350+Arvind9792@users.noreply.github.com>
2023-12-20 20:02:35 +0000

    array done

---
+++ 02_basic/01_array.js
@@ -0,0 +1,22 @@
+//push
+const arr=new Array (1,2,3,4,5)
+console.log(arr);
+arr.push(6)
+console.log(arr);
+//pop()
+const arr2= new Array(1,3,4,6,8)
+const arr3=arr2.pop()
+
+console.log(arr2);
+console.log(arr3);
+//unshift
+
+const arr4=new Array (44,55,555,99,'khjk')
+arr4.unshift('arvind')
+console.log(arr4)
+arr4.shift()
+console.log(arr4 )
+
+//include()
+const arr6=new Array("Arvind",9,4,5,5,3,4,3)
+console.log(arr6.includes(5));
